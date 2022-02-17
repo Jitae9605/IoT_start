c350d840a33f01de0318ec1e3aca9c081e6bcddd
Jitae9605 <dlwlxo3819@naver.com>
2022-02-17 14:18:29 +0900

    Update 21_ ~~~.c

---
+++ IoT_start/source/Class,lecture/21_sub_3_line.h
@@ -0,0 +1,8 @@
+#include "21_sub_3_point.h"		// Point 구조체포함
+
+typedef struct
+{
+	Point first;				// 첫 번째점
+	Point second;				// 두 번째점
+
+} Line;
---
+++ IoT_start/source/Class,lecture/21_sub_3_point.h
@@ -0,0 +1,10 @@
+#ifndef _POINT_H_			// _POINT_H_ 매크로명이 정의되어있지않으면
+#define _POINT_H_			// _POINT_H_ 매크로명을 정의한다.
+
+typedef struct
+{
+	int x;				// x좌표
+	int y;				// y좌표
+} Point;
+
+#endif						// #ifndef _POINT_H_ 종료
